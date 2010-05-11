cc3d37e4d70bc8372e254e1f53dede6d6cfbe4e0
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-05-11 22:24:05 +0200

    Flussdiagramme (noch nicht fertig)

--- IT2/aufgabe32a.asm
+++ IT2/aufgabe32a.asm
@@ -1,9 +1,12 @@
 	TITLE "Aufgabe32a"
 	ORG %0C
+	; Initialiere Port 0 fuer Ausgabe
 	LD P01M, #%0
+	; Definiere alle Bits von Port 2
+	; als Eingabe-Bits	
 	LD P2M,  #%FF
+	; Setze Port 2 auf Push-Pull
 	LD P3M,  #%1
-	
 	begin:
 	LD 0, 2
 	JP begin
