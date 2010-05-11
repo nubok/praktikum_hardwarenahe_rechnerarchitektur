b124e413762eba69cde559ca78def83910240597
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-05-11 14:49:46 +0200

    Aenderungen im Abschnitt zu Zaehlen von Anzahl Umschaltungen.

--- IT2/aufgabe32a.asm
+++ IT2/aufgabe32a.asm
@@ -1,8 +1,8 @@
 	TITLE "Aufgabe32a"
 	ORG %0C
-	LD P01M,  #%0
-	LD P2M, #%FF
-	LD P3M, #%1
+	LD P01M, #%0
+	LD P2M,  #%FF
+	LD P3M,  #%1
 	
 	begin:
 	LD 0, 2
--- IT2/aufgabe32c_ohneschwingung.asm
+++ IT2/aufgabe32c_ohneschwingung.asm
@@ -21,21 +21,6 @@
 	CP 5, 6
 	JP Z, begin
 
-	; Wir wollen verhindern, dass es durch 
-	; Schwingungen entstanden ist
-	; 0x10 (r0): auessere Schleife (zum Warten)
-	; 0x11 (r1): innere Schleife (zum Warten)
-
-	LD RP, #%10
-
-	; Schleifen 
-	LD r0, #%FF
-	outer_loop:
-	LD r1, #%FF
-	inner_loop:
-	DJNZ r1, inner_loop
-	DJNZ r0, outer_loop
-
 	INC 4
 	LD P0, 4
 	LD 5, 6
